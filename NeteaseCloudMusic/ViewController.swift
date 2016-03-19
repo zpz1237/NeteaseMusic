b67cf012f3311c56c842870e214979e206a21218
NSNirvana <NSNirvana@iCloud.com>
2016-03-19 18:10:29 +0800

    Initial Commit

---
+++ NeteaseCloudMusic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NeteaseCloudMusic
+//
+//  Created by Nirvana on 3/19/16.
+//  Copyright © 2016 NSNirvana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
