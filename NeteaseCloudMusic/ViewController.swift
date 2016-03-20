cfbbda3f890c15a87dd7e5d165dd08a32a4e0caa
NSNirvana <NSNirvana@iCloud.com>
2016-03-20 22:47:12 +0800

    I'm an idiot

--- NeteaseCloudMusic/ViewController.swift
+++ NeteaseCloudMusic/ViewController.swift
@@ -10,16 +10,86 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private var surroundings = UIBarButtonItem()
-    private var music = UIBarButtonItem()
+    private let surroundings = UIBarButtonItem()
+    private let music = UIBarButtonItem()
+    private var pageView: CAPSPageMenu?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         addLeftAndRightBarButtonItem()
         hideNavBarHairLineImageView()
+        addPageMenu()
     }
 
+    /**
+     添加主PageView
+     */
+    func addPageMenu() {
+        let array: [(String,String)] = [
+            ("PersonalizeVC","个性推荐"),
+            ("ListVC","歌单"),
+            ("BroadcastVC","主播电台"),
+            ("TopVC","排行榜")
+        ]
+        
+        let controllerArray = instantiateViewControllerWithIdentifiersAndTitles(array)
+        let parameters = offerCAPSPageMenuOptionsAccordingToNumber(array.count)
+        
+        pageView = CAPSPageMenu(viewControllers: controllerArray, frame: CGRectMake(0.0, 64.0, self.view.frame.width, self.view.frame.height-64), pageMenuOptions: parameters)
+        self.view.addSubview(pageView!.view)
+    }
+    
+    /**
+     提供pageView配置参数
+     
+     - returns: 配置参数集
+     */
+    func offerCAPSPageMenuOptionsAccordingToNumber(number: Int) -> [CAPSPageMenuOption] {
+        let menuItemWidth: CGFloat = 75.0
+        
+        let screenWidth = UIScreen.mainScreen().bounds.width
+        let menuMargin = (screenWidth - CGFloat(number)*menuItemWidth)/CGFloat(number+1)
+        
+        let parameters: [CAPSPageMenuOption] = [
+            .MenuItemSeparatorWidth(0),
+            .ScrollMenuBackgroundColor(UIColor(red: 248.0/255.0, green: 248.0/255.0, blue: 248.0/255.0, alpha: 1.0)),
+            .ViewBackgroundColor(UIColor(red: 240.0/255.0, green: 240.0/255.0, blue: 240.0/255.0, alpha: 1.0)),
+            .BottomMenuHairlineColor(UIColor(red: 20.0/255.0, green: 20.0/255.0, blue: 20.0/255.0, alpha: 0.1)),
+            .SelectionIndicatorColor(UIColor(red: 206.0/255.0, green: 42.0/255.0, blue: 33.0/255.0, alpha: 1.0)),
+            .MenuMargin(menuMargin),
+            .MenuHeight(39.0),
+            .MenuItemWidth(menuItemWidth),
+            .SelectedMenuItemLabelColor(UIColor(red: 206.0/255.0, green: 42.0/255.0, blue: 33.0/255.0, alpha: 1.0)),
+            .UnselectedMenuItemLabelColor(UIColor(red: 40.0/255.0, green: 40.0/255.0, blue: 40.0/255.0, alpha: 1.0)),
+            .MenuItemFont(UIFont(name: "HelveticaNeue", size: 15.0)!),
+            .UseMenuLikeSegmentedControl(false),
+            .MenuItemSeparatorRoundEdges(true),
+            .SelectionIndicatorHeight(2.0),
+            .MenuItemSeparatorPercentageHeight(0.1)
+        ]
+        return parameters
+    }
+    
+    /**
+     传入标识符以及名称实例化VC
+     
+     - parameter dictionary: [(identifier,title)]
+     
+     - returns: 实例化后的VCs
+     */
+    func instantiateViewControllerWithIdentifiersAndTitles(array: [(String,String)]) -> [UIViewController] {
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        var controllerArray: [UIViewController] = []
+        
+        for i in 0 ..< array.count {
+            let vc = storyBoard.instantiateViewControllerWithIdentifier(array[i].0)
+            vc.title = array[i].1
+            controllerArray.append(vc)
+        }
+        return controllerArray
+    }
+    
     /**
      隐藏shadowImage并保持NavBar的半透明性
      */
