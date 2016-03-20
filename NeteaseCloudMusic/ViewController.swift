f4d1043414b16ade32efccee4ed2b544e576f429
NSNirvana <NSNirvana@iCloud.com>
2016-03-20 11:38:27 +0800

    fuck the shadowImage

--- NeteaseCloudMusic/ViewController.swift
+++ NeteaseCloudMusic/ViewController.swift
@@ -10,14 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var surroundings = UIBarButtonItem()
-    var music = UIBarButtonItem()
+    private var surroundings = UIBarButtonItem()
+    private var music = UIBarButtonItem()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        addLeftAndRightBarButtonItem()
+        hideNavBarHairLineImageView()
+    }
+
+    /**
+     隐藏shadowImage并保持NavBar的半透明性
+     */
+    func hideNavBarHairLineImageView() {
+        let navBarHairLineImageView = findNavBarHairLineImageView(self.navigationController!.navigationBar)
+        navBarHairLineImageView!.hidden = true
+    }
+    
+    /**
+     递归查找shadowImage
+     
+     - parameter view: 包含shadowImage的父View
+     
+     - returns: shadowImage
+     */
+    func findNavBarHairLineImageView(view: UIView) -> UIImageView? {
+        if view is UIImageView && view.bounds.size.height <= 1 {
+            return view as! UIImageView
+        }
+        for subview in view.subviews {
+            if let imageView = findNavBarHairLineImageView(subview) {
+                return imageView
+            }
+        }
+        return nil
+    }
+    
+    /**
+     添加左右按钮
+     */
+    func addLeftAndRightBarButtonItem() {
         surroundings.image = UIImage(named: "iconfont-tinggeshiqu")?.imageWithRenderingMode(.AlwaysOriginal)
         music.image = UIImage(named: "iconfont-yinle")?.imageWithRenderingMode(.AlwaysOriginal)
+        
+        surroundings.target = self
+        music.target = self
+
+        surroundings.action = "clickedLeftBarButtonItem"
+        music.action = "clickedRightBarButtonItem"
         
         let negativeSpacer = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FixedSpace, target: nil, action: nil)
         negativeSpacer.width = -8
@@ -25,11 +66,19 @@ class ViewController: UIViewController {
         self.navigationItem.leftBarButtonItems = [negativeSpacer, surroundings]
         self.navigationItem.rightBarButtonItems = [negativeSpacer, music]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    /**
+     听歌识曲
+     */
+    func clickedLeftBarButtonItem() {
+        print("left")
     }
-
+    
+    /**
+     正在播放
+     */
+    func clickedRightBarButtonItem() {
+        print("right")
+    }
 }
 
